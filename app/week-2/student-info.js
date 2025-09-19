a27728208009b9cf515bebd3c8cc2b91c0529f6c
Amal Musse <amalmusse@MacBook-Pro-2.local>
2025-09-19 07:26:10 -0600

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Amal Musse</p>
+            <p>
+                GitHub:{" "}
+                <Link href="https://github.com/MusseTech/cprg306-assignments" target="_blank">
+                    cprg306-assignments
+                </Link>
+            </p>
+        </div>
+    );
+}
+
+
+
+

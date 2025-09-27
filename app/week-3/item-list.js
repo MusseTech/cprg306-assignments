9e23098140475dc56c091d6038f88f57af69e9bb
MusseTech <Amal.Musse@edu.sait.ca>
2025-09-26 22:30:14 -0600

    Fix: debug ItemList import and rendering in Shopping List page

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import Item from './item'; // Import the Item component
+
+const itemList = [
+    { name: "milk, 4 L 🥛", quantity: 1,category: "dairy" },
+    { name: "bread 🍞", quantity: 2, category: "bakery" },
+    { name: "eggs, dozen 🥚", quantity: 2, category: "dairy" },
+    { name: "bananas 🍌", quantity: 6, category: "produce" },
+    { name: "broccoli 🥦", quantity: 3, category: "produce" },
+    { name: "chicken breasts, 1 kg 🍗", quantity: 1, category: "meat" },
+    {name: "pasta sauce 🍝", quantity: 3, category: "canned goods" },
+    {name: "spaghetti, 454 g 🍝", quantity: 2, category: "dry goods"},
+    {name: "toilet paper, 12 pack 🧻",quantity: 1,category: "household" },
+    { name: "paper towels, 6 pack", quantity: 1, category: "household" },
+    { name: "dish soap 🍽️", quantity: 1, category: "household" },
+    { name: "hand soap 🧼", quantity: 4,category: "household" },
+];
+const ItemList = () => {
+    return (
+        <ul className="mt-8">
+            <item 
+            kety={index}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+            />
+        </ul>
+    );
+};
+
+export default ItemList;
